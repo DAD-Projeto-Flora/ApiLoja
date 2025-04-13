83a178cc90845b4c5a3a9b30e796703175315bb4
melpiress <melissa.fonseca@germinare.org.br>
2025-04-13 18:06:38 -0300

    transformando o generation type do id de produto em identity

--- flora/src/main/java/com/flora/Model/ProdutoModel.java
+++ flora/src/main/java/com/flora/Model/ProdutoModel.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "produto")
 public class ProdutoModel {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
 
