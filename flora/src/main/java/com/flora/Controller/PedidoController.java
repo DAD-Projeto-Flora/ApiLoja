a5a4db7cceea1ab12c3ba3782dd22bdbd00f8c79
Brenosilva13 <breno.souza@germinare.org.br>
2025-04-13 01:54:19 -0300

    adicionado cross-origin

--- flora/src/main/java/com/flora/Controller/PedidoController.java
+++ flora/src/main/java/com/flora/Controller/PedidoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "https://site-loja-flora.vercel.app")
 @RestController
 @RequestMapping("/order")
 public class PedidoController {
--- flora/src/main/java/com/flora/Controller/ProdutoController.java
+++ flora/src/main/java/com/flora/Controller/ProdutoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "https://site-loja-flora.vercel.app")
 @RestController
 @RequestMapping("/product")
 public class ProdutoController {
